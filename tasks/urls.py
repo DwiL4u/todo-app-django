2bf74c5fe45b64b564771c14476059484421445a
Dwi Lenggani <dwilenggani25@gmail.com>
2024-12-22 17:30:31 +0900

    Add initial project files and database

---
+++ tasks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+      path('', views.index, name='tasks'),
+
+      path ('update-task/<str:pk>/', views.updateTask, name='update-task'),
+      
+      path ('delete-task/<str:pk>/', views.deleteTask, name='delete-task'),
+      
+
+]
